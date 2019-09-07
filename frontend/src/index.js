992378673af722c65bacd71e7fe9f76ac87dfc36
srafi1 <shakilrafi0@gmail.com>
2019-09-07 17:04:15 -0400

    materialize and navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,38 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Navbar, NavItem } from 'react-materialize';
+
 import Info from './components/Info';
 import Dashboard from './components/Dashboard';
 import Earned from './components/Earned';
 import Spent from './components/Spent';
 import Net from './components/Net';
 
+const nav = (
+  <Navbar brand={<a href="/">&nbsp;PennApes</a>} alignLinks="right">
+    <NavItem href="/dashboard">
+      Dashboard
+    </NavItem>
+    <NavItem href="/spent">
+      Spent
+    </NavItem>
+    <NavItem href="/earned">
+      Earned
+    </NavItem>
+    <NavItem href="/net">
+      Net
+    </NavItem>
+    <NavItem href="/">
+      Logout
+    </NavItem>
+  </Navbar>
+);
+
 const router = (
   <Router>
+    {nav}
+
     <Route exact path="/" component={Info} />
     <Route exact path="/dashboard" component={Dashboard} />
     <Route exact path="/spent" component={Spent} />
