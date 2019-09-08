22cf4f798e32888a4390b7f931e37a4c4c5724c2
Jasper Cheung <wymanjasper@gmail.com>
2019-09-08 04:52:17 -0400

    Net gain

--- frontend/src/components/Net.jsx
+++ frontend/src/components/Net.jsx
@@ -1,58 +1,78 @@
 import React, { Component } from 'react';
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine, Label
 } from 'recharts';
 
 const data = [
   {
-    name:'3/1', bal: 10,
+    name:'3/1', Total: 15,
   },
   {
-    name:'3/2', bal: -10,
+    name:'3/2', Total: -10,
   },
    {
-    name:'3/3', bal: -10,
+    name:'3/3', Total: -10,
    },
    {
-    name:'3/4', bal: 10,
+    name:'3/4', Total: 14,
    },
    {
-    name:'3/5', bal: -10,
+    name:'3/5', Total: -1,
    },
    {
-    name:'3/6', bal: 10,
+    name:'3/6', Total: 2,
    },
    {
-     name:'3/7', bal: -10,
+     name:'3/7', Total: -15,
    },
+   {
+    name:'3/8', Total: 15,
+  },
+  {
+    name:'3/9', Total: -10,
+  },
+   {
+    name:'3/10', Total: -10,
+   },
+   {
+    name:'3/11', Total: 14,
+   },
+   {
+    name:'3/12', Total: -1,
+   },
+   {
+    name:'3/13', Total: 2,
+   },
+   {
+     name:'3/14', Total: -15,
+   },
+
 
 ];
 class Net extends Component {
 
   render() {
     return (
-      <div>
-        <p>Net</p>
-        <div>
-          <BarChart data={data}width={600} height={200} type={"bar"}
+      <div style={{display:'flex', flexDirection:'row', alignItems:'center', height: '500px', paddingRight:"50px"}}  >
+          <BarChart data={data}width={750} height={200} type={"bar"}
           >
             < XAxis dataKey="name"
                     tickLine={false}
                     axisLine={false}
                     />
-            <YAxis />
+            <YAxis width={50}> <Label value="$" /> </YAxis>
+        />
             <ReferenceLine y={0} stroke="#000" />
-
-       <Bar dataKey="bal">
+            <Tooltip/>
+       <Bar dataKey="Total">
             {
                 data.map((entry, index) => {
-                  const result = entry.bal >  0 ? '#33cc33' : '#cc3333';
+                  const result = entry.Total >  0 ? '#33cc33' : '#cc3333';
                   return <Cell fill={result} />;
                 })
               }
             </Bar>
           </BarChart>
-        </div>
       </div>
     );
   }
